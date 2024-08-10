c12bdacce935e079dae01bcad4418c0891d35fe3
Raiyan Hasan <raiyanhasan2006@gmail.com>
2024-08-10 16:42:07 +0530

    updated

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-const aiList=["gemini","chatgpt","google"];
-let ai
+// const aiList=["ollama","jarvis","friday","hacksmith","rayan"]
 
-const showAiList= (arr)=>{
+let aiList;
+
+
+// show available ai list
+const showAiList =(arr)=>{
+    let aiListItem = document.querySelector(".ai-list-item");
+    console.log(aiListItem);
+    aiListItem.innerHTML="";
     arr.forEach(element => {
-        
+        let p = document.createElement("p");
+        p.textContent = element;
+        aiListItem.appendChild(p);
     });
 }
+
+
+showAiList(aiList)
