1e345cb547cd67252d8169d47edd777f0252cd33
Raiyan Hasan <raiyanhasan2006@gmail.com>
2024-08-10 17:08:14 +0530

    available ai list offline added support

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
- const aiList=["ollama","jarvis","friday","hacksmith","rayan"]
+let aiList=[];
+
+async function getAiList() {
+    const url = "http://localhost:11434/api/tags";
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        
+        console.log(json);
+        return json
+        } catch (error) {
+        console.error(error.message);
+    }
+}
+// Using .then()
+getAiList().then(data => {
+    console.log(data.models.length)
+    for (i=0;i<data.models.length;i++){
+        aiList.push(data.models[i].name);
+    }
+    showAiList(aiList)
+});
 
 
 // show available ai list
-const showAiList =(arr)=>{
+const showAiList = (arr) => {
     let aiListItem = document.querySelector(".ai-list-item");
     console.log(aiListItem);
-    aiListItem.innerHTML="";
+    aiListItem.innerHTML = "";
     arr.forEach(element => {
         let p = document.createElement("p");
         p.textContent = element;
@@ -14,4 +39,4 @@ const showAiList =(arr)=>{
 }
 
 
-showAiList(aiList)
+
